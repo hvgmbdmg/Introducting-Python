3dde4752054e5dfa43791d70c57c2bf4e4de8326
Albert <hvgmbdmg@gmail.com>
2019-03-25 00:35:52 +0800

    Import module Sample

---
+++ main.py
@@ -0,0 +1,14 @@
+import aaaaa.bbb.fun1st as f1
+
+# page 3
+quotes = {
+"Moe": "A wiseguy, huh?",
+"Larry": "Ow!",
+"Curly": "Nyuk nyuk!"
+}
+
+stooge = "Curly"
+print(stooge, "says:", quotes[stooge])
+
+
+f1.fun1()
